3f453d642d8157204e0e9cc0bf554fe3d1768fac
Phillip Ramos <phillipramos@Phillips-MacBook-Pro.local>
2016-08-02 19:18:55 -0400

    Updated server.js and added public folder and app.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var sessions = require('sessions');
 
 // Database Configuration
 // require mongojs, then save the url of our database 
 // name of our collection
 var mongojs = require('mongojs');
-var databaseUrl = require('breaking-news');
-var collections = require('news');
+var databaseUrl = 'breaking-news';
+var collections = ['news'];
 
 // Use mongojs to hook the database to the db variable 
 var db = mongojs(databaseUrl, collections);
@@ -19,6 +19,23 @@ var db = mongojs(databaseUrl, collections);
 // Logs any errors if mongodb has any issues
 db.on('error', function(err) {
   console.log('Database Error:', err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// set app to run at port 3000
+app.listen(3000, function() {
+  console.log('App running on port 3000!')
 });
 
 
