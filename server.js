afe37abc7aba533d0510483ffbcfd6fcd1e405d4
Phillip Ramos <phillipramos@Phillips-MacBook-Pro.local>
2016-08-02 22:25:44 -0400

    added my routes and html paths.Also updated server.js some more

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var PORT = process.env.PORT || 3000;
 
 // Database Configuration
 // require mongojs, then save the url of our database 
@@ -21,21 +22,21 @@ db.on('error', function(err) {
   console.log('Database Error:', err);
 });
 
+// Express to handle data parsing
+app.use(express.static('app/public/'));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+// Routes
+require('./app/routes/data-routes/data.js')(app,db);
+require('./app/routes/html-routes/html.js')(app,db);
 
-
-
-
-
-
-
-
-
-
-
-// set app to run at port 3000
-app.listen(3000, function() {
-  console.log('App running on port 3000!')
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
 });
 
 
